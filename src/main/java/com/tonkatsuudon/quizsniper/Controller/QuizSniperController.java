f030c734d1d06eec1829b96951b29bb440c76e29
Yota Momose <tonkatsu440@gmail.com>
2024-08-25 16:46:42 +0900

    設定画面への遷移を機能を追加

--- src/main/java/com/tonkatsuudon/quizsniper/Controller/QuizSniperController.java
+++ src/main/java/com/tonkatsuudon/quizsniper/Controller/QuizSniperController.java
@@ -8,9 +8,17 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class QuizSniperController {
+    /* 初期表示（メイン画面） */
     @GetMapping("/")
     public ModelAndView showMainView(ModelAndView mv) {
         mv.setViewName("index");
+        return mv;
+    }
+
+    /* 設定画面遷移 */
+    @GetMapping("/setting")
+    public ModelAndView showSettingView(ModelAndView mv) {
+        mv.setViewName("setting");
         return mv;
     }
     
