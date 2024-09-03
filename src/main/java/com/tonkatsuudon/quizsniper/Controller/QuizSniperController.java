1be09e4a66c0ccbbf090ef18b55c44dc703a0102
Yota Momose <tonkatsu440@gmail.com>
2024-09-03 18:39:25 +0900

    テーブルからのデータ取得を実装

--- src/main/java/com/tonkatsuudon/quizsniper/Controller/QuizSniperController.java
+++ src/main/java/com/tonkatsuudon/quizsniper/Controller/QuizSniperController.java
@@ -1,17 +1,30 @@
 package com.tonkatsuudon.quizsniper.controller;
 
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.tonkatsuudon.quizsniper.entity.Users;
+import com.tonkatsuudon.quizsniper.repository.UsersRepository;
+
+import lombok.AllArgsConstructor;
+
 
 @Controller
+@AllArgsConstructor
 public class QuizSniperController {
+    private final UsersRepository usersRepository;
     /* 初期表示（メイン画面） */
     @GetMapping("/")
     public ModelAndView showMainView(ModelAndView mv) {
+        
+
         mv.setViewName("index");
+        List<Users> usersList = usersRepository.findAll();
+        mv.addObject("usersList", usersList);
         return mv;
     }
 
---
+++ src/main/java/com/tonkatsuudon/quizsniper/repository/UsersRepository.java
@@ -0,0 +1,11 @@
+package com.tonkatsuudon.quizsniper.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tonkatsuudon.quizsniper.entity.Users;
+
+@Repository
+public interface UsersRepository extends JpaRepository<Users,String> {
+
+}
