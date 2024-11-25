636474d5147635e948a0d8a6e50d8ccd3ae03914
Yota Momose <tonkatsu440@gmail.com>
2024-11-25 21:01:57 +0900

    新規登録時のバリデーションチェックと入力欄の最大文字数を追加

--- src/main/java/com/tonkatsuudon/quizsniper/form/RegisterData.java
+++ src/main/java/com/tonkatsuudon/quizsniper/form/RegisterData.java
@@ -3,14 +3,23 @@ package com.tonkatsuudon.quizsniper.form;
 import com.tonkatsuudon.quizsniper.entity.Users;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class RegisterData {
     @NotBlank(message = "ユーザーIDを入力して下さい")
+    @Pattern(
+        regexp = "^[a-zA-Z0-9]{5,20}$",
+        message = "IDは半角英数字で5～20文字にしてください。"
+    )
     private String userId;
 
     @NotBlank(message = "パスワードを入力して下さい")
+    @Pattern(
+        regexp = "^[a-zA-Z0-9]{5,20}$",
+        message = "パスワードは半角英数字で5～20文字にしてください。"
+    )
     private String password;
 
     /**
